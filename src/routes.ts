f5088955955349e002fc1aff32a9d78ce1aef478
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-19 14:50:21 +0300

    createa the google auth 2.0

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import {Express, Request, Response} from "express";
 import {createUserHandler} from "./controller/user.controller";
 import validateResourse from "./middleware/validateResourse";
 import {createUserSchema} from "./schema/user.schema";
-import {createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler} from "./controller/session.controller";
+import {createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler, googleOauthHandler} from "./controller/session.controller";
 import {createSessionSchema} from "./schema/session.schema";
 import requireUser from "./middleware/requireUser";
 import { createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema } from "./schema/product.schema";
@@ -13,6 +13,7 @@ function routes (app: Express) {
 
     app.post('/api/users', validateResourse(createUserSchema), createUserHandler)
     app.post('/api/sessions', validateResourse(createSessionSchema), createUserSessionHandler)
+    app.get('/api/sessions/oauth/google', googleOauthHandler)
     app.get('/api/sessions', requireUser, getUserSessionsHandler)
     app.delete('/api/sessions', requireUser, deleteSessionHandler)
 
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,12 +1,15 @@
 import {DocumentDefinition, FilterQuery} from 'mongoose';
 import { omit } from 'lodash'
+import qs from 'querystring'
 import UserModel, {UserDocument} from "../models/user.model";
+import config from 'config'
+import axios from 'axios'
+import log from '../utils/logger'
 
 interface IvalidatePassword {
     email: string;
     password: string
 }
-
 
 export async function createUser (data: DocumentDefinition<Omit<UserDocument, 'createdAt' | 'updatedAt' | 'comparePassword'>>) {
     try {
@@ -29,4 +32,70 @@ export async function validatePassword({email, password}: IvalidatePassword) {
 
 export async function findUser(query: FilterQuery<UserDocument>) {
     return UserModel.findOne(query).lean()
+}
+
+interface GoogleTokensResult {
+  access_token: string;
+  expires_in: Number;
+  refresh_token: string;
+  scope: string;
+  id_token: string;
+}
+
+export async function getGoogleOAuthTokens({
+  code,
+}: {
+  code: string;
+}): Promise<GoogleTokensResult> {
+  const url = "https://oauth2.googleapis.com/token";
+
+  const values = {
+    code,
+    client_id: config.get("googleClientId"),
+    client_secret: config.get("googleClientSecret"),
+    redirect_uri: config.get("googleOauthRedirectUrl"),
+    grant_type: "authorization_code",
+  };
+
+  try {
+    const res = await axios.post<GoogleTokensResult>(
+      url,
+      qs.stringify(values),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    return res.data;
+  } catch (error: any) {
+    console.error(error.response.data.error);
+    log.error(error, "Failed to fetch Google Oauth Tokens");
+    throw new Error(error.message);
+  }
+}
+
+export async function getGoogleUser({ id_token, access_token}) {
+  try {
+    const res = await axios.get(
+      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${id_token}`,
+        },
+      }
+    );
+    return res.data;
+  } catch (error: any) {
+    log.error(error, "Error fetching Google user");
+    throw new Error(error.message);
+  }
+}
+
+export async function findAndUpdateUser(
+  query: FilterQuery<UserDocument>,
+  update: UpdateQuery<UserDocument>,
+  options: QueryOptions = {}
+) {
+  return UserModel.findOneAndUpdate(query, update, options);
 }
